2f487fe5f2e66a91d30ac6f5353ec5e60b992621
AshuraOP <115545269+AshuraOP@users.noreply.github.com>
2026-01-28 19:17:54 +0530

    demo

---
+++ DayOne/src/com/tnsif/dayone/Demo1.java
@@ -0,0 +1,14 @@
+package com.tnsif.dayone;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        int $num1 = 20;
+        System.out.println("Value of the number is :" + $num1);
+
+        String studentName = "Aditya";
+        System.out.println("Student name is :" + studentName);
+
+        char Gender = 'M';
+        System.out.println("Gender :" + Gender);
+    }
+}
--- DayOne/src/com/tnsif/dayone/FirstProgram.java
+++ DayOne/src/com/tnsif/dayone/FirstProgram.java
@@ -4,6 +4,16 @@ public class FirstProgram {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello Everyone");
+		int number = 12;
+		System.out.println(number);
+		int $num1 = 20;
+		System.out.println("Value of the number is :" + $num1);
+
+		String studentName = "Aditya";
+		System.out.println("Student name is :" + studentName);
+
+		char Gender = 'M';
+		System.out.println("Gender :" + Gender);
 	}
 
 }
