67bafaf38168df72e182d4f419d8bbe77e127c65
AshuraOP <115545269+AshuraOP@users.noreply.github.com>
2026-04-07 15:14:38 +0530

    a

---
+++ day2/src/LoopsDemo/ForEachLoopDemo.java
@@ -0,0 +1,24 @@
+package LoopsDemo;
+
+public class ForEachLoopDemo {
+
+	public static void main(String[] args) {
+
+		int a[]= {10,20,30,40,50};
+		
+		//for integer array
+		for (int storeNormalData : a)
+		{
+			System.out.print(storeNormalData+ " ");
+		}
+		
+		System.out.println();
+		
+		char ch[]= {'j','a','v','a'};
+		for(char c: ch)
+		{
+			System.out.print(c);
+		}
+	}
+
+}
---
+++ day2/src/LoopsDemo/WhileLoopDemo.java
@@ -0,0 +1,20 @@
+package LoopsDemo;
+public class WhileLoopDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i=105;
+		
+		while(i<=100)
+		{
+			System.out.println(i);
+			i++;
+		}
+		System.out.println("----------------");
+		
+		
+		
+	}
+
+}
